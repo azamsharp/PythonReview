2920d2e24e599877e0e508053d153e8ede29f7b1
Mohammad Azam <azamsharp@gmail.com>
2021-12-10 10:01:12 -0600

    added mul, div functions to calculator

---
+++ main.py
@@ -0,0 +1,37 @@
+
+# MODULES 
+#import utils 
+#import utils as u 
+
+# import everything from utils 
+from utils import * 
+import calculator 
+
+print("Enter 1 for palindrome: ")
+print("Enter 2 for even/odd: ")
+print("Enter 3 for prime: ")
+
+#choice = int(input("Enter choice: ")) 
+
+#print(utils.is_palindrome("cat")) 
+#print(utils.is_even(4)) 
+
+#u.is_prime(10)
+
+#print(is_palindrome("cat")) 
+
+print(calculator.add(1,5)) 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ utils.py
@@ -0,0 +1,9 @@
+
+def is_palindrome(word): 
+  return word == "mom"
+
+def is_even(number): 
+  return number % 2 == 0 
+
+def is_prime(number): 
+  return number == 3 
